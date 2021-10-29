2b1549588573414ef94f085e2b2cc43fe57520e4
Hector Canizales <heror0484@protonmail.com>
2021-10-28 19:15:25 -0500

    working release, see src/cjson/main.c

--- src/cjson/main.c
+++ src/cjson/main.c
@@ -25,13 +25,13 @@ int main(){
 		return( -1 );
 	}
 	// printf("file is...\n%s\t EOF \n", buffer);
-	printf("length is %i\n", len);
+	// printf("length is %i\n", len);
 
 	cJSON *json_parsed = cJSON_Parse(buffer);
 
 	int array_size = cJSON_GetArraySize(json_parsed);
 
-	printf("Total events: %i\n", array_size);
+	// printf("Total events: %i\n", array_size);
 
 	// 
 	cJSON_ArrayForEach(events, json_parsed){
@@ -54,13 +54,22 @@ int main(){
 			if (strcmp( country -> valuestring, \
 					"Mexico") == 0 ) {
 				name_event = cJSON_GetObjectItemCaseSensitive(events, "name");	
-				printf("Found event in Mexico.\t%s\n", name_event -> valuestring );
+				// printf("Found event in Mexico.\t%s\n", name_event -> valuestring );
 				//     resolutions = cJSON_AddArrayToObject(monitor, "resolutions");
 				// even
 				// object cJSON= events_mx 
 				// cJSON_AddItemToArray(events_array, events)
 				// cJSON_AddObjectToObject(events_array, events);
-				cJSON_AddArrayToObject(events_array, events);
+				//cJSON_AddObjectToObject(events_mx_events, events);
+				//
+				//
+				int arr_size = cJSON_GetArraySize(events_mx_events);
+				 //printf("Items; %d \n", arr_size);
+
+				//cJSON_InsertItemInArray(events_mx_events, arr_size+1 , events);
+				// cJSON_AddItemToArray(events_mx_events, events);
+				// if AddItemToArray is used, the  ArrayForEach stops working...
+				cJSON_AddItemReferenceToArray(events_mx_events, events);
 
 
 					
@@ -72,7 +81,7 @@ int main(){
 
 
 	char * stringed = cJSON_Print(events_mx);
-	printf("new is... %s", stringed);
+	printf("%s", cJSON_Print(events_mx) );
 
 	return(0);
 
