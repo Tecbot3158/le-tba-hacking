446fd9218f06902ac994bca900e3ef18d9f83eac
Hector Canizales <heror0484@protonmail.com>
2021-11-02 20:03:28 -0600

    add example using library

--- src/cjson/downloader/tba_downloader.c
+++ src/cjson/downloader/tba_downloader.c
@@ -17,90 +17,13 @@ int main(){
 		.endpoint = "status",
 	} ;
 
-	CURL *tba_url_handle;
+	FILE * myfile = perform_get_tba( & test );
 
-	curl_global_init(CURL_GLOBAL_ALL);
+	char line[100];	
 
-	tba_url_handle = curl_easy_init();
-
-
-
-	char ** bufloc = NULL;
-	size_t data_size = 0;
-	FILE * data_file = open_memstream(bufloc, & data_size );
-	size_t header_size = 0;
-	FILE * header_file = open_memstream(bufloc, & header_size );
-
-
-	char * tba_key = getenv("TBA_AUTH_KEY");
-	char header_auth[300];
-	sprintf(header_auth, "%s %s", tba_base_header, tba_key);
-
-
-
-	//printf("Header is %s\n", header_auth);
-
-
-	struct curl_slist * headers_list=NULL;
-	headers_list=curl_slist_append( headers_list, header_auth);
-
-
-	if(headers_list == NULL){
-		printf("error!\n");
-		return -1;
+	while ( fgets( line, sizeof line, myfile) != NULL ) {
+		printf("%s", line);
 	}
-
-	char target_url_c[150];
-	char * target_url = target_url_c;
-	sprintf(target_url_c, "%s/%s", tba_base_url, "status");
-
-	//printf("Target url is %s\n", target_url);
-
-
-	curl_easy_setopt( tba_url_handle, CURLOPT_URL, target_url);
-	curl_easy_setopt( tba_url_handle, CURLOPT_HTTPHEADER, headers_list);
-
-	curl_easy_setopt( tba_url_handle, CURLOPT_WRITEFUNCTION, write_data);
-
-	curl_easy_setopt( tba_url_handle, CURLOPT_HEADERDATA, header_file);
-	curl_easy_setopt( tba_url_handle, CURLOPT_WRITEDATA, data_file);
-
-
-	curl_easy_perform( tba_url_handle );
-	// curl_easy_clean( )
-
-	char s[1000];
-
-	while ( fgets(s, sizeof s, data_file) != NULL ){
-		printf("%s", s);
-	}
-
-	
-
-	printf("--------------\n");
-	printf("lullz header\n");
-
-	// while ( fgets(s, sizeof s, header_file) != NULL ){
-	// 	printf("%s", s);
-	// }
-
-	char response[1000];
-
-	fscanf(header_file, "HTTP/2 %s", response);
-	//fgets(response, sizeof response, header_file);
-
-	printf("HTTP response code is %s\n", response);
-
-		exit(0);
-
-
-	fclose(	data_file);
-	fclose( header_file);
-
-
-
-	// printf("Key is\n%s\n", tba_key);
-
 
 
 }
@@ -156,8 +79,10 @@ FILE * perform_get_tba (TBA_request * request) {
 
 	curl_easy_perform( url_handle );
 
+	char s[100];
+
 	// while ( fgets(s, sizeof s, header_file) != NULL ){
-	// 	printf("%s", s);
+	//  	printf("%s", s);
 	// }
 
 	char response[100];
@@ -165,16 +90,23 @@ FILE * perform_get_tba (TBA_request * request) {
 	fscanf(header_file, "HTTP/2 %s", response);
 	//fgets(response, sizeof response, header_file);
 	//
+	printf("response is... %s\n", response);
 	request->http_response = atoi(response);
 
-	fclose(	data_file);
-	fclose( header_file);
+ 	curl_easy_cleanup( url_handle );
 
-	curl_easy_cleanup( url_handle );
+	//free(header_file);
+	//fclose(	data_file);
+	//fclose( header_file);
+	
+	free(header_file);
 
-	return data_file;
 
-	return NULL;
+
+
+	return data_file ;
+
+	// return NULL;
 }
 
 
